48748493492e9763fce2fefca0e43a59e0438cbc
unknown <erik@computer.com>
2021-04-14 20:42:43 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav>
+          <h1>Hang tight!</h1>
+          <h3>My portfolio is on it's way. In the meantime...</h3>
+          <h5>Play <a href="https://www.erikgrivalsky.com/baristamax">BaristMax</a></h5>
+        </nav>
       </header>
     </div>
   );
