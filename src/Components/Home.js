cb57d45aef49b4fb44202c9388fec93f5a63291f
unknown <erik@computer.com>
2021-04-16 16:18:39 -0600

    resume placement styling and formatting

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div class="container">
             <h1 class='container'>Home</h1>
             <main class="container">
 
