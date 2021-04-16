86531095853dbd880503903e22f8c4558cf5ad5e
unknown <erik@computer.com>
2021-04-16 12:28:26 -0600

    Adds Navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
+import './Navbar';
 
 function App() {
   return (
     <div>
       <header>
-        <nav>
-          <h1>Hang tight!</h1>
+          <Navbar />
+          
+
+      </header>
+      <body>
+      <h1>Hang tight!</h1>
           <h3>My portfolio is on it's way. In the meantime...</h3>
           <h5>Play <a href="https://www.erikgrivalsky.com/baristamax">BaristMax</a></h5>
-        </nav>
-      </header>
+      </body>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>About</li>
+                <li>Portfolio</li>
+                <li>Resume</li>
+                <li>Contact</li>
+                <li>Github</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Navbar;
