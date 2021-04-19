220213408c475ec8148f93f71771081d318b14b0
unknown <erik@computer.com>
2021-04-19 13:20:02 -0600

    adds downloadable resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,6 +4,7 @@ const Resume = () => {
     return (
         <div>
             <h1 class='container'>Resume</h1>
+            <h2><a href="https://www.erikgrivalsky.com/portfolio/static/resume_grivalsky.pdf" target="_blank" download>Download resume here</a></h2>
             <main class='container' id='resume-container'>
                 <iframe title="An Embedded Copy of my Resume" id="resume" src="https://docs.google.com/document/d/e/2PACX-1vQe6E-rtuZ104cWhlLZ1BtfmIuGj2YaeMShlJzZlbHI35H-1_8LDR6LuUMYK75yD_sQs-U8dvtTY7s2/pub?embedded=true" height="1240" width="900"></iframe>
             </main>
