d4eae0c704f9a5fe9981132eb248cff0abb24d03
unknown <erik@computer.com>
2021-06-13 14:47:36 -0400

    adds photo

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 class='container'>Erik Grivalsky - Full-Stack Software Developer</h1>
-            <main class="container">
-                <span id="about-me-text">I am a full stack web developer whose background in live comedy theater and customer service management motivates me to continue working in teams to achieve a singular goal. My tendency to seek novel points of view helps me solve problems by approaching them from several angles. My skills include Javascript, NoeJS, Express, React, and Python.</span>
+            <h1 class='container' id="title">Erik Grivalsky - Full Stack Software Developer</h1>
+            <main class="container" id="main-container">
+                <img src="https://res.cloudinary.com/dbljwcjis/image/upload/v1623609330/erikg_qu2iik.png" alt="photo of erik" id="photo-of-erik"></img>
+                <span id="about-me-text">I am a full stack software developer whose background in live comedy theater and customer service management motivates me to continue working in teams to achieve a singular goal. My tendency to seek novel points of view helps me solve problems by approaching them from several angles. My skills include Javascript, NoeJS, Express, React, and Python.</span>
             </main>
         </div>
     );
