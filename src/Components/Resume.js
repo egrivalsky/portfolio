214464836d5a36f35ef78aa91b0efaba72ab8215
unknown <erik@computer.com>
2021-07-08 11:47:15 -0400

    updates resume

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
             <h1 class='container'>Resume.</h1>
             <h2><a href="https://www.erikgrivalsky.com/portfolio/static/resume_grivalsky.pdf" target="_blank" download>Download resume here</a></h2>
             <main class='container' id='resume-container'>
-                <iframe title="An Embedded Copy of my Resume" id="resume" src="https://docs.google.com/document/d/e/2PACX-1vQe6E-rtuZ104cWhlLZ1BtfmIuGj2YaeMShlJzZlbHI35H-1_8LDR6LuUMYK75yD_sQs-U8dvtTY7s2/pub?embedded=true" height="1240" width="900"></iframe>
+                <iframe title="An Embedded Copy of my Resume" id="resume" src="https://docs.google.com/document/d/e/2PACX-1vSMKjomNwDwM8YbCl6GjHjjziUxQREbXwkr0KtdA6I2ngQVC7HWvpF2fx4ywmx6FUsc2mQL-Mnr-7co/pub?embedded=true" height="1240" width="900"></iframe>
             </main>
         </div>
     );
